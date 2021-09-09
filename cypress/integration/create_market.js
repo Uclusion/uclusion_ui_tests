67fb08d1953da498fdaf2931aabf31e09c19ae01
disrael <disrael@gmail.com>
2021-09-09 09:49:44 -0700

    feat: now can test signup locally

--- cypress/integration/create_market.js
+++ cypress/integration/create_market.js
@@ -1,40 +1,45 @@
-/* eslint-disable */
-import localforage from 'localforage';
 
 describe('Authenticator:', function() {
+  const destination = 'https://stage.uclusion.com';
   // Step 1: setup the application state
   beforeEach(function() {
-    cy.visit('http://stage.uclusion.com');
+
   });
 
   describe('Check market creation', () => {
-    it('signs in and creates market and verifies', () => {
-      // Step 2: Take an action (Sign in)
-      cy.get(selectors.usernameInput).type("ethan.israel@uclusion.com");
-      cy.get(selectors.signInPasswordInput).type("5dpE7ykHaenW2pi");
-      cy.get(selectors.signInSignInButton).contains('Sign In').click();
-      cy.get('.MuiGrid-grid-md-1 > .MuiButtonBase-root > .MuiButton-label').click();
-      cy.get(selectors.firstOption).contains('Document').click();
-      let todayDate = Cypress.moment().format('MMM DD, YYYY');
-      let marketName = 'Automate test' + todayDate;
-      cy.get('#name').type(marketName);
-      cy.get('.jss358 > .MuiButtonBase-root > .MuiButton-label').click();
-      cy.get('.ql-editor').type("something text");
-      cy.get('.jss361 > .MuiButton-label').click();
-      cy.get('.jss358 > .MuiButton-outlined').click();
-      cy.get('.jss801').click()
-      cy.get('.jss641').click();
-      cy.get(':nth-child(5) > .MuiButtonBase-root > .MuiButton-label').click();
+    it('signs up and creates template market and verifies', () => {
+      const testStartDate = new Date();
+      cy.visit(`${destination}?utm_campaign=test#signup`);
+      cy.get('#name').type('Tester Uclusion');
+      cy.get('#email').type('tuser@uclusion.com');
+      cy.get('#password').type('Testme;1');
+      cy.get('#repeat').type('Testme;1');
+      cy.get('#terms').click();
+      cy.get('#signupButton').click();
+      // Make the 🧙‍ happen!
+      cy.task("gmail:check", {
+        from: "support@uclusion.com",
+        to: "tuser@uclusion.com",
+        subject: "Please verify your email address",
+        after: testStartDate,
+        include_body: true
+      }).then(emails => {
+        assert.isNotNull(emails, 'No email returned');
+        assert.isNotEmpty(emails, 'Email was not found');
+        assert.lengthOf(emails, 1, 'Too many emails - maybe concurrent tests');
+        const searchString = emails[0].body.html;
+        const begin = searchString.indexOf(`href="${destination}`) + 6;
+        const end = searchString.indexOf('"', begin);
+        const url = searchString.substring(begin, end);
+        cy.visit(url, {failOnStatusCode: false});
+        cy.get('#username').type('tuser@uclusion.com');
+        cy.get('#password').type('Testme;1');
+        cy.get('#signinButton').click();
+        //TODO create template market and verify by doing tour
+        //TODO invite someone to the template market and have them signup and verify in when tour works
+        //TODO sign out at the end so that don't collide with other tests
+      });
     });
   });
 
 });
-export const selectors = {
-  // Auth component classes
-  usernameInput: '#username',
-  signInPasswordInput: '#password',
-  signInSignInButton: '.MuiButton-label',
-  firstOption: '.jss344 > .MuiContainer-root > .MuiPaper-root >' +
-    ' :nth-child(2) > :nth-child(1) > .MuiButtonGroup-root > :nth-child(1) > .MuiButton-label'
-
-};
---
+++ cypress/plugins/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const gmail = require("gmail-tester");
+const util = require("util");
+
+module.exports = (on, config) => {
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+    on("task", {
+        "gmail:check": async args => {
+            let done_waiting_time = 0;
+            do {
+                const emails = await gmail.get_messages(
+                    path.resolve(__dirname, "credentials.json"), // credentials.json is inside plugins/ directory.
+                    path.resolve(__dirname, "gmail_token.json"), // gmail_token.json is inside plugins/ directory.
+                    args
+                );
+                if (emails.length > 0) {
+                    console.log(`[gmail] Found!`);
+                    return emails;
+                }
+                done_waiting_time += 5;
+                if (done_waiting_time >= 60) {
+                    console.log("[gmail] Maximum waiting time exceeded!");
+                    return null;
+                }
+                await util.promisify(setTimeout)(5000);
+            } while (true);
+        }
+    });
+};
