496fc33e3f0191e5219e85bfd2198cbd0c4e744e
disrael <disrael@gmail.com>
2021-09-30 19:21:39 -0700

    fix: wait for market creation to finish

--- cypress/integration/create_market.js
+++ cypress/integration/create_market.js
@@ -25,7 +25,7 @@ function createAndTourWorkspace() {
   cy.get('#OnboardingWizardContinue', { timeout: 20000 }).click();
   cy.get('#workspaceName').type('Workspace created from UI tests');
   cy.get('#OnboardingWizardFinish').click();
-  cy.get('#addressAddCancel').click();
+  cy.get('#addressAddCancel', { timeout: 20000 }).click();
   cy.get('[title=Next]', { timeout: 20000 }).click();
   cy.get('[title=Next]').click();
   cy.get('[title=Next]').click();
