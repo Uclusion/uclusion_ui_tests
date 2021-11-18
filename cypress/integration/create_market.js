53dc43a8d1efc6a71b0f422cd0882d1217134930
disrael <disrael@gmail.com>
2021-11-17 19:02:58 -0800

    fix: timeouts for loading market stages

--- cypress/integration/create_market.js
+++ cypress/integration/create_market.js
@@ -28,11 +28,12 @@ function createAndTourWorkspace() {
 }
 
 function takeInvitedTour(isCreator) {
+  // Need the timeouts because market can still be loading
   cy.get('[title=Next]', { timeout: 5000 }).click();
   if (!isCreator) {
-    cy.get('[title=Next]').click();
+    cy.get('[title=Next]', { timeout: 5000 }).click();
   }
-  cy.get('[title=Close]').first().click();
+  cy.get('[title=Close]', { timeout: 5000 }).first().click();
 }
 
 function waitForEmail(userEmail, destination, subject) {
