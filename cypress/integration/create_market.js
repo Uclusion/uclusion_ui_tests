c56b3eadde28975ef485252b1671659140760ff4
disrael <disrael@gmail.com>
2021-11-17 21:21:04 -0800

    fix: increase time after finish on first wizard

--- cypress/integration/create_market.js
+++ cypress/integration/create_market.js
@@ -29,7 +29,7 @@ function createAndTourWorkspace() {
 
 function takeInvitedTour(isCreator) {
   // Need the timeouts because market can still be loading
-  cy.get('[title=Next]', { timeout: 5000 }).click();
+  cy.get('[title=Next]', { timeout: 8000 }).click();
   if (!isCreator) {
     cy.get('[title=Next]').click();
   }
