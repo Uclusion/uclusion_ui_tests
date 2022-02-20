edd81152b1979efe98858df6204bc451f193da0b
disrael <disrael@gmail.com>
2022-02-19 18:01:56 -0800

    fix: inbox row present but not visible so compensate

--- cypress/integration/create_market.js
+++ cypress/integration/create_market.js
@@ -137,7 +137,7 @@ describe('Authenticator:', function() {
         takeInvitedTour(false);
         cy.get('#Jobs').click();
         cy.get('#swimLanesChildren').contains('Creating this story to test placeholder gets it', { timeout: 20000 }).click();
-        cy.get('li').filter(':visible').contains('Certain', { timeout: 5000 });
+        cy.get('span').filter(':visible').contains('Certain', { timeout: 5000 });
       });
     });
   });
