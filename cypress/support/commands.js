99d20737f5f44573c31dd4ea02e8d8f8e8531108
disrael <disrael@gmail.com>
2022-02-28 14:34:51 -0800

    feat: start on demo

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -137,7 +137,7 @@ Cypress.Commands.add("createJob", (name, description, assigneeName, certainty, j
             cy.get(`#editorBox-${marketId}-planning-inv-add`).type(description);
         }
         if (certainty) {
-            cy.get(`input[value=${certainty}]`).should('be.visible').click();
+            cy.get(`#${certainty}`).click();
         }
         if (justification) {
             cy.get(`#editorBox-${marketId}-add-initial-vote`).type(justification);
