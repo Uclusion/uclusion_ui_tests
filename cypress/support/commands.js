7c5ab79affacd8f36ac55f5178d1e179e3226bc8
disrael <disrael@gmail.com>
2022-03-01 23:29:32 -0800

    feat: start on demo

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -151,7 +151,7 @@ Cypress.Commands.add("createJob", (name, description, assigneeName, certainty, j
         cy.get('#Description', {timeout: 10000}).should('be.visible');
         if (isReady) {
             cy.get('#readyToStartCheckbox').click();
-            cy.get('#readyToStartCheckbox').contains('[value=true]', {timeout: 10000});
+            cy.get('#readyToStartCheckbox').contains('[type="checkbox"]', {timeout: 10000}).should('have.value', true);
         }
     });
 })
