2aa41d4d902926cb16d0af29aa10a030b1975193
disrael <disrael@gmail.com>
2022-03-15 19:46:21 -0700

    fix: add fully voted

--- cypress/integration/create_demo3.js
+++ cypress/integration/create_demo3.js
@@ -39,7 +39,6 @@ describe('Demo 2:', function() {
       cy.createQuestionOption('Zoom', 'The communications tool for online meetings.', false);
       //Notification: new option submitted
       cy.createQuestionOption('Integration testing for Lambdas', undefined, false, false, false, false);
-      cy.get(returnToChannel).click();
       cy.navigateIntoJob('Upgrade Material UI');
       cy.voteSuggestion(true, 50, 'Depends on effort estimate.');
     });
