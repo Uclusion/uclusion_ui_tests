6dabdaa9421e1b1f94464d15ac039efaffc9da3a
disrael <disrael@gmail.com>
2022-04-29 11:21:00 -0700

    fix: add test for following url to question option ie subscribe route

--- cypress/integration/create_market.js
+++ cypress/integration/create_market.js
@@ -36,11 +36,10 @@ describe('Authenticator:', function() {
           cy.contains(optionText, {timeout: 120000}).click();
           cy.get("#shareButtonExplanation", { timeout: 5000 }).click();
         });
-        cy.get('#inviteLinker', { timeout: 5000 }).within(() =>
-            cy.find('input').then((input) => {
+        cy.get('#inviteLinker', { timeout: 5000 }).find('input')
+            .then((input) => {
               optionUrl = input.attr('value');
-            })
-        );
+            });
         cy.get('#AddCollaborators').click();
         return cy.get('#inviteLinker', { timeout: 5000 }).find('input');
       }).then(input => {
