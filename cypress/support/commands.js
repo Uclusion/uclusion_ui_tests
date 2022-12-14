def24139338edee32c57c98e578613130d588e74
disrael <disrael@gmail.com>
2022-12-14 14:03:23 -0800

    fix: upgrade cypress and drop real events which were only necessary if wanted clip board in Chrome

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,7 +24,6 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import "cypress-real-events/support";
 import {cleanAccount} from "./fixture_management";
 
 Cypress.Commands.add("cleanAccount", (userConfiguration) => {
