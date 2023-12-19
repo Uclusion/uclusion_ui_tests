f8f412b20af41e49abf2c0858ae41fcc206a5605
disrael <disrael@gmail.com>
2023-12-19 11:28:17 -0800

    fix: clear referred tusers from previous runs so now find in drop down by email

--- cypress/e2e/invite_from_demo.js
+++ cypress/e2e/invite_from_demo.js
@@ -37,8 +37,7 @@ describe('Authenticator:', function() {
                 cy.get('#OnboardingWizardNext').click();
                 cy.get('[id^=editorBox-reply]').type('Would really love your opinion @',
                     { delay: 500 });
-                // Unfortunately not clearing previous runs demo markets so the name is in the system for this email
-                cy.get('li[data-value="Tester Two Uclusion"]').click();
+                cy.get(`li[data-value="${secondUserEmail}"]`).click();
                 cy.get('#OnboardingWizardNext').click();
                 // Make sure back in inbox and done with operation before click anything
                 cy.get('#ForYou', { timeout: 8000 }).should('exist');
