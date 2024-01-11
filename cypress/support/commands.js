40808e8ea32a0facdec2d09e6045c5aa6694dd82
disrael <disrael@gmail.com>
2024-01-11 10:48:13 -0800

    fix: correct the notification count

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -154,7 +154,7 @@ Cypress.Commands.add("confirmDemoMarketInbox", (description) => {
     const inboxContents = [{notification: 'UNREAD_JOB_APPROVAL_REQUEST', count: 2},
         {notification: 'UNREAD_COMMENT', count: 1}, {notification: 'UNASSIGNED', count: 1},
         {notification: 'NOT_FULLY_VOTED', count: 2}, {notification: 'REPLY_MENTION', count: 1},
-        {notification: 'REVIEW_REQUIRED', count: 1}];
+        {notification: 'REVIEW_REQUIRED', count: 1}, {notification:'UNREAD_REVIEWABLE', count: 1}];
     inboxContents.forEach((content) => {
         const { notification, count } = content;
         cy.get(`[id^=workListItem${notification}]`).should('have.length', count);
