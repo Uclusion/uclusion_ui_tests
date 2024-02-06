391b4193eaafe5e99e05430b5be01d1b21c95ac9
disrael <disrael@gmail.com>
2024-02-06 14:55:34 -0800

    fix: timing of support notification

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -151,7 +151,8 @@ Cypress.Commands.add("createSuggestion", (description) => {
 Cypress.Commands.add("confirmDemoMarketInbox", () => {
     cy.get('#workspaceFromDemoBanner', { timeout: 30000 }).should('exist');
     cy.get('#Everyone', { timeout: 30000 }).should('exist');
-    cy.get('[id^=workListItemREVIEW_REQUIRED]', { timeout: 30000 }).should('exist');
+    // Special case the support notification as it is not from quick add like the rest
+    cy.get('[id^=workListItemUNREAD_COMMENT]', { timeout: 30000 }).should('have.length', 2);
     const inboxContents = [{notification: 'UNREAD_JOB_APPROVAL_REQUEST', count: 3},
         {notification: 'UNREAD_COMMENT', count: 2}, {notification: 'UNASSIGNED', count: 1},
         {notification: 'NOT_FULLY_VOTED', count: 2}, {notification: 'REPLY_MENTION', count: 1},
