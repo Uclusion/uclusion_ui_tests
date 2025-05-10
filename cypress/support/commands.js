06f6fd804056479733caeb1817be4c4230c7390c
disrael <disrael@gmail.com>
2025-05-10 07:46:20 -0700

    fix: ready for assignment pull not push

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -152,8 +152,7 @@ Cypress.Commands.add("confirmDemoMarketInbox", () => {
     const inboxContents = [{notification: 'UNREAD_JOB_APPROVAL_REQUEST', count: 1},
         {notification: 'UNREAD_COMMENT', count: 3}, {notification: 'UNASSIGNED', count: 1},
         {notification: 'UNREAD_ESTIMATE', count: 1}, {notification: 'NOT_FULLY_VOTED', count: 2},
-        {notification: 'REPLY_MENTION', count: 1}, {notification: 'REVIEW_REQUIRED', count: 1},
-        {notification:'UNREAD_REVIEWABLE', count: 2}];
+        {notification: 'REPLY_MENTION', count: 1}, {notification: 'REVIEW_REQUIRED', count: 1}];
     inboxContents.forEach((content) => {
         const { notification, count } = content;
         cy.get(`[id^=workListItem${notification}]`).its('length').should('be.gte', count);
